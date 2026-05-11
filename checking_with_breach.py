21c8d73a80a762e1ea28927bf8ea90ef4bbb4a30
azzahzayn <azzahzaynh.b23cs2124@mbcet.ac.in>
2026-05-11 19:02:54 +0530

    Add files via upload
    Initial commit - strength checker + breach detection

---
+++ checking_with_breach.py
@@ -0,0 +1,54 @@
+import re #to do pattern checking
+import hashlib
+import requests
+def checkstrength(pw):
+    score=0
+    feedback=[] 
+    if len(pw)>=12:
+        score+=2
+    elif len(pw)>=8:
+        score+=1
+    else:
+        feedback.append('password too short make it 12 characters')
+    if re.search(r'[A-Z]',pw):
+        score+=1
+    else:
+        feedback.append('password should contain atleast one uppercase character')
+    if re.search(r'[a-z]',pw):
+        score+=1
+    else:
+        feedback.append('password should contain atleast one lowercase character')
+    if re.search(r'[0-9]',pw):
+        score+=1
+    else:
+        feedback.append('password should contain atleast one digit')
+    if(re.search(r'[!@#$%^&*()_+={}|:;"<>?/]',pw)):
+        score+=1
+    else:
+        feedback.append('password should contain atleast one special character')
+    if(score>=6):
+        print('strong pw')
+    elif(score>=3):
+        print('medium pw')
+    else:
+        print('weak pw')
+    for i in feedback:
+        print(i)
+    return     
+def checkbreach(pw):
+    hashed=hashlib.sha1(pw.encode()).hexdigest().upper()
+    prefix=hashed[:5]
+    url=f"https://api.pwnedpasswords.com/range/{prefix}"
+    response=requests.get(url)
+    if(response.status_code==200):
+        values=response.text.splitlines()
+        for i in values:
+            suffix,count=i.split(':')
+            if(suffix==hashed[5:]):
+                print('no of times breach has happened ->',count)
+                return 
+    print('no breach has happened')
+    return
+password=input()
+checkstrength(password)
+checkbreach(password)
